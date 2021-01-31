8a7f7cdaae8cfea00ebc1b1b91c77f408a8e072e
fankai <git@fankai.dev>
2021-01-31 18:24:22 +0800

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello fankaidev</h1>
+      <h1>Hello fankai</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
